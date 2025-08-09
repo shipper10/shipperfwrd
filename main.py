4d13836110542874837506aaae24ad7693799a11
shipper10 <87245353+shipper10@users.noreply.github.com>
2025-08-09 14:05:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,43 @@
-import asyncio
+import os
 from telethon import TelegramClient, events
+
+# تأكد من إنشاء مجلد للجلسة في المسار الذي تعمل فيه
+session_dir = 'sessions'
+os.makedirs(session_dir, exist_ok=True)
+session_name = os.path.join(session_dir, 'my_session')
 
 # بيانات التوثيق
 api_id = '26075519'
 api_hash = '5819201f8de7de4ea548335e78a59696'
 phone_number = '+249904085742'
-session_name = '1BJWap1sBu4B_MI3Ons8go-nv9Fk5TAekMcl0GoG2it9aZqw7BP7ZKIpem39ZgjIj2qCMyFKVMOipqB8I0UnP_lGP839MKadaCsi9qyT8o4F7bpS6eDNoOs-x3DM1dM7eSIIxx4sJj7-dVhJ4BlsLQvnrsqNnG2pb1JjM5tIkWI0GjVWuHXevOpcXwt94nIF_I3tgTZbosbUEf9yM2767-jhOaFB2D_6snf0zP5J122IMX3uG49HX7q0HBj0Q-W2q5n-QcPGGnvaVsTtNdpg27t-YnpkfkYJwUOkLAE9rNjJwi3TPtQATDy_AX6BBWp0OrK8wHCcTt76AGvx77BimCkY5-HX4WTE='  # اسم الجلسة المحفوظة
 
 # المعرفات الخاصة بالقنوات/المجموعات
 source_channel_ids = [
     '-1001595923708',
     '-1001668684235',
-    # أضف المزيد من القنوات هنا
 ]
-target_chat_id = '-1002686274384'  # المعرف الخاص بالقناة/المجموعة المستهدفة
+target_chat_id = '-1002686274384'
 
 # إنشاء العميل باستخدام الجلسة المحفوظة
 client = TelegramClient(session_name, api_id, api_hash)
 
-# قائمة لتخزين الرسائل التي تم توجيهها لمنع التكرار
-forwarded_messages = set()
-
 async def main():
-    # تسجيل الدخول تلقائيًا باستخدام الجلسة المحفوظة
     await client.start(phone_number)  # سيتم التوثيق تلقائيًا إذا كانت الجلسة موجودة
     print("Logged in successfully.")
 
     # الحدث الذي يستمع للرسائل الجديدة من القنوات المحددة
     @client.on(events.NewMessage(chats=source_channel_ids))
     async def handler(event):
-        # تحقق من أن الرسالة لم تُرسل مسبقًا (لتجنب التكرار)
+        # تحقق من أن الرسالة لم تُرسل مسبقًا
         if event.message.id not in forwarded_messages:
             try:
                 # إعادة توجيه الرسالة إلى القناة/المجموعة المستهدفة
                 await event.forward_to(target_chat_id)
-                # إضافة المعرف إلى مجموعة الرسائل المحفوظة لمنع التكرار
+                # إضافة المعرف إلى مجموعة الرسائل المحفوظة
                 forwarded_messages.add(event.message.id)
                 print(f"Forwarded message: {event.message.id}")
             except Exception as e:
                 print(f"Error forwarding message: {e}")
-        else:
-            print(f"Message {event.message.id} already forwarded, skipping.")
 
     # تشغيل البوت بشكل مستمر
     await client.run_until_disconnected()
